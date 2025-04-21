7f42e9a23e8754d42b87893281f7eea3ac3e6007
0Prince-Gupta0 <prince17072003@gmail.com>
2025-04-21 22:25:56 +0530

    config changes03

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,11 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+
 
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/redirects',
-          dest: '.', // This will be renamed and copied to /dist/_redirects
-          rename: '_redirects',
-        }
-      ]
-    })
   ],
   define: {
     global: 'window', // 👈 this fixes the error
