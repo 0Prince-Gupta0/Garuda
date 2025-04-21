da9fd65eeac3a58a1d2e58712ea1acc82a58c383
0Prince-Gupta0 <prince17072003@gmail.com>
2025-04-21 22:18:25 +0530

    config changes01

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   plugins: [
